be814b88de00bd8d546d416f23d385a161dba228
Aditya Jyoti <aj.adityajyoti@gmail.com>
2025-12-30 17:13:46 +0530

    feat: add spicetify config

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -18,26 +18,26 @@ end
 -- Load once on startup
 source_matugen()
 
---------------------------------------------------
--- Reload on Matugen updates (SIGUSR1)
---------------------------------------------------
+-- --------------------------------------------------
+-- -- Reload on Matugen updates (SIGUSR1)
+-- --------------------------------------------------
 
-vim.api.nvim_create_autocmd("Signal", {
-  pattern = "SIGUSR1",
-  callback = function()
-    source_matugen()
+-- vim.api.nvim_create_autocmd("Signal", {
+--   pattern = "SIGUSR1",
+--   callback = function()
+--     source_matugen()
 
-    -- Reload lualine because base16 overrides highlights
-    pcall(function()
-      require("lualine").setup({
-        options = { theme = "base16" },
-      })
-    end)
+--     -- Reload lualine because base16 overrides highlights
+--     pcall(function()
+--       require("lualine").setup({
+--         options = { theme = "base16" },
+--       })
+--     end)
 
-    -- Optional stylistic tweaks
-    vim.api.nvim_set_hl(0, "Comment", { italic = true })
-  end,
-})
+--     -- Optional stylistic tweaks
+--     vim.api.nvim_set_hl(0, "Comment", { italic = true })
+--   end,
+-- })
 
 --------------------------------------------------
 -- Fix lualine inverted colors (keep this)
